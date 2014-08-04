d074db254502e94e2630a9112900e21ca9ee5fdb
xTRiM <rt@onpay.ru>
2014-08-04 23:38:48 +0400

    Set proper versions

--- recipes/percona-devel.rb
+++ recipes/percona-devel.rb
@@ -1,8 +1,11 @@
 include_recipe "percona::package_repo"
 
-package "Percona-Server-devel-56" do
+package "Percona-Server-client-56" do
   action :install
 end
-package "Percona-Server-client-56" do
+package "Percona-Server-server-56" do
+  action :install
+end
+package "Percona-Server-devel-56" do
   action :install
 end
