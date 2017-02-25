64fbbde810031b7b8a2474747ec45025405fd3b9
Andrey Ignatov <a.ignatov@dointeractive.ru>
2017-02-25 23:04:36 +0300

    Update broken source link

--- recipes/msfonts.rb
+++ recipes/msfonts.rb
@@ -1,12 +1,12 @@
-remote_file "/usr/local/src/msttcore-fonts-2.0-3.noarch.rpm" do
-  source "http://www.my-guides.net/en/images/stories/fedora12/msttcore-fonts-2.0-3.noarch.rpm"
+remote_file "/usr/local/src/msttcore-fonts-2.6-1.noarch.rpm" do
+  source "https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm"
   #action :create_if_missing
-  not_if "rpm -qa | grep -qx 'msttcore-fonts-2.0-3.noarch'"
+  not_if "rpm -qa | grep -qx 'msttcore-fonts-2.6-1.noarch'"
 end
 
-package "msttcore-fonts-2.0-3.noarch" do
+package "msttcore-fonts-2.6-1.noarch" do
   action :install
-  source "/usr/local/src/msttcore-fonts-2.0-3.noarch.rpm"
+  source "/usr/local/src/msttcore-fonts-2.6-1.noarch.rpm"
   options "--nogpgcheck"
   #notifies :run, "bash[update-alternatives java]", :immediately
 end
